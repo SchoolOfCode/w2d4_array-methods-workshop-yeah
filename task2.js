fa9a5ec1b17e829fa32d8a6b5a840669856b6f52
YeganaAbd <abd.yegane@gmail.com>
2021-11-25 15:24:15 +0000

    Task 2 and Task 4

--- task2.js
+++ task2.js
@@ -1,11 +1,23 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+
+function startB(letter) {
+  let firstLetter = letter.charAt(0);
+  //console.log(firstLetter);
+
+  return firstLetter === "b";
+}
+
+let bListAnimals = animals.filter(startB);
+console.log(bListAnimals);
+
+// You have an array of lovely animals. However, you can only keep the animals that start with the letter `b`.
+
+// 👉 2a. In the new variable `bListAnimals`, use the `.filter()` method to generate a new array which contains only the animals whose name begins with 'b'.
--- task4.js
+++ task4.js
@@ -1,3 +1,42 @@
-let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+// 👉 4a. Using the `.every()` method, check that every number in the `sevenTimesTable` array is a multiple of `7`.
 
+// 👉 4b. Check if every number in the `seventySevenTimesTable` array is a multiple of `77` using the `.every()` method.
+
+// 👉 4c. Your worst fears have been confirmed. The result of step 4b showed you that there is a rogue value in your `seventySevenTimesTable` array. Use the `.find()` method to identify which number it is.
+
+// 👉 4d. Now that you know which number is incorrect, use the `.findIndex()` method to identify the index of the number.
+
+let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
+// Task 4a
+function isMultipleOfSeven(number) {
+  return number % 7 === 0;
+}
+let multipleOfSeven = sevenTimesTable.filter(isMultipleOfSeven);
+console.log(multipleOfSeven);
+
+// Task 4b
+function isMultipleOfSeventySeven(number) {
+  return number % 77 === 0;
+}
+
+let multipleOfSeventySeven = seventySevenTimesTable.filter(
+  isMultipleOfSeventySeven
+);
+console.log(multipleOfSeventySeven);
+
+// Task 4c
+
+function findMultipleOfSeventySeven(number) {
+  return number % 77 !== 0;
+}
+
+let findSeventySeven = seventySevenTimesTable.find(findMultipleOfSeventySeven);
+console.log(findSeventySeven);
+
+// Task 4d
+let findIndexSeventySeven = seventySevenTimesTable.findIndex(
+  findMultipleOfSeventySeven
+);
+console.log(findIndexSeventySeven);
